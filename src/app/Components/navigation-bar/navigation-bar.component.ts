7530f6cf54e7065f76ae683eb5476061567affef
CHAVDAYASHVANTSINH612 <84241638+CHAVDAYASHVANTSINH612@users.noreply.github.com>
2024-09-01 18:11:24 +0530

    initial commit

---
+++ src/app/Components/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBars,
+  faBarsProgress,
+  faCoffee,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrl: './navigation-bar.component.css',
+})
+export class NavigationBarComponent {
+  @ViewChild('ulElement') ulElement!: ElementRef;
+
+  isMenuOpen = false;
+  faBars = faBars;
+  faXmark = faXmark;
+  menuIcon = faBars;
+
+  toggleFunc(): void {
+    console.log('hello');
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (this.isMenuOpen) {
+      this.menuIcon = this.faXmark; // Change icon to X mark
+      this.ulElement.nativeElement.classList.add('showDropDown');
+      console.log(this.ulElement.nativeElement);
+    } else {
+      this.menuIcon = this.faBars; // Change icon back to bars
+      this.ulElement.nativeElement.classList.remove('showDropDown');
+      console.log(this.ulElement.nativeElement);
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavigationBarComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
