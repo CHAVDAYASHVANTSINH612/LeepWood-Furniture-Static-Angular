7c860d544138da5bea2bf45c8d5161dd2e486ac1
CHAVDAYASHVANTSINH612 <84241638+CHAVDAYASHVANTSINH612@users.noreply.github.com>
2024-09-14 22:06:30 +0530

    Action button design changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProductsComponent } from './Components/products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    HomeComponent,
+    ProductsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
