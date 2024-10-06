3289596609c0eacde16b13a20201d6a4de393b42
CHAVDAYASHVANTSINH612 <84241638+CHAVDAYASHVANTSINH612@users.noreply.github.com>
2024-10-06 18:20:47 +0530

    Added location button at contactus page and 2. changed location button at navbar

--- src/app/Components/contact-us/contact-us.component.ts
+++ src/app/Components/contact-us/contact-us.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
 })
-export class ContactUsComponent  implements OnInit {
-
-  constructor() { }
-
+export class ContactUsComponent implements OnInit {
+  constructor() {}
+  faLocationDot = faLocationDot;
   ngOnInit() {}
-
 }
