6d148db1d1a46baedf55ba8d27f42cfed8b9f016
CHAVDAYASHVANTSINH612 <84241638+CHAVDAYASHVANTSINH612@users.noreply.github.com>
2025-02-02 12:30:17 +0530

    1. Made product-sofa component more reusable 2. added location gandhinagar on home component

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import {
+  faLocationDot,
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  faLocationDot = faLocationDot;
 }
